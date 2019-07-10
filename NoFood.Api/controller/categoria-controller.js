b3245a5cdae5acd66889a9a3e94308855cbef13b
PattySantos <patriciadeo.santos@gmail.com>
2019-07-09 23:43:04 -0300

    criamos os schemas de categoria e produtos

--- NoFood.Api/controller/categoria-controller.js
+++ NoFood.Api/controller/categoria-controller.js
@@ -1,22 +1,31 @@
 'use strict'
+require('../models/categoria-model');
+const mongoose = require('mongoose');
+const categoria = mongoose.model('Categoria');
+
 function categoriaController(){
 
 }
 categoriaController.prototype.post = async (req, res)=>{
+    let modelo = new categoria(req.body);
+    return modelo.save();
 
  };
  categoriaController.prototype.put = async (req, res)=>{
+    await categoria.findByIdAndUpdate(req.params.id, {$set: req.body});
+    return categoria.findById(req.params.id);
 
 };
 categoriaController.prototype.get = async (req, res)=>{
-    res.status(200).send('funcionando...listando as categorias');
+    return categoria.find();
 
 };
 categoriaController.prototype.getById = async (req, res)=>{
-    res.status(200).send(`O id passado foi ${req.params.id}`);
+   return categoria.findById(req.params.id);
 
 };
 categoriaController.prototype.delete = async (req, res)=>{
+    return categoria.findByIdAndRemove(req.params.id);
 
 };
 
